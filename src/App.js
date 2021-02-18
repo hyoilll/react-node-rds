7e8071541d95e88417f954aab350bc42a57e3404
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-02-18 19:14:47 +0900

    create react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="gray-backgroud">
+      <img src={logo} lat="logo"></img>
+      <h2>hello world</h2>
+    </div>
+  );
+}
+
+export default App;
