94438e49f40d8fe8049b17300108e090e98fa8e6
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-02-19 18:13:09 +0900

    Add Material UI Design for Add/Delete

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,6 +1,20 @@
 import React, { useState } from "react";
 import { post } from "axios";
 
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  hidden: {
+    display: "none",
+  },
+}));
+
 const CustomerAdd = ({ stateRefresh }) => {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState("");
@@ -8,6 +22,19 @@ const CustomerAdd = ({ stateRefresh }) => {
   const [birthday, setBirthday] = useState("");
   const [gender, setGender] = useState("");
   const [job, setJob] = useState("");
+  const [open, setOpen] = useState(false); //dialog 창이 열려있는지 체크
+
+  const classes = useStyles();
+
+  const initState = () => {
+    setFile(null);
+    setFileName("");
+    setUserName("");
+    setBirthday("");
+    setGender("");
+    setJob("");
+    setOpen(false);
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -17,12 +44,7 @@ const CustomerAdd = ({ stateRefresh }) => {
       stateRefresh();
     });
 
-    setFile(null);
-    setFileName("");
-    setUserName("");
-    setBirthday("");
-    setGender("");
-    setJob("");
+    initState();
   };
 
   const addCustomer = () => {
@@ -43,6 +65,16 @@ const CustomerAdd = ({ stateRefresh }) => {
     };
 
     return post(url, formData, config);
+  };
+
+  //고객 추가 모달 창이 열리면
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  //고객 추가 모달 창이 닫히면
+  const handleClose = () => {
+    initState();
   };
 
   const handleFileChange = (e) => {
@@ -69,51 +101,125 @@ const CustomerAdd = ({ stateRefresh }) => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h1>Add Customer</h1>
-      Profile Image:{" "}
-      <input
-        type="file"
-        name="file"
-        file={file}
-        value={fileName}
-        onChange={handleFileChange}
-      ></input>
-      <br />
-      Name:{" "}
-      <input
-        type="text"
-        name="userName"
-        value={userName}
-        onChange={(e) => handleValueChange(e, "userName")}
-      ></input>
-      <br />
-      Birthday:{" "}
-      <input
-        type="text"
-        name="birthday"
-        value={birthday}
-        onChange={(e) => handleValueChange(e, "birthday")}
-      ></input>
-      <br />
-      Sex:{" "}
-      <input
-        type="text"
-        name="gender"
-        value={gender}
-        onChange={(e) => handleValueChange(e, "gender")}
-      ></input>
-      <br />
-      Job:{" "}
-      <input
-        type="text"
-        name="job"
-        value={job}
-        onChange={(e) => handleValueChange(e, "job")}
-      ></input>
-      <br />
-      <button type="submit">Add</button>
-    </form>
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClickOpen}>
+        Add Customer
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle> Add Customer</DialogTitle>
+        <DialogContent>
+          <input
+            className={classes.hidden}
+            accept="image/*"
+            id="raised-button-file"
+            type="file"
+            file={file}
+            value={fileName}
+            onChange={handleFileChange}
+          ></input>
+          <br />
+          <label htmlFor="raised-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              name="file"
+            >
+              {fileName === "" ? "Select Profile Image" : fileName}
+            </Button>
+          </label>
+          <br />
+          <TextField
+            label="userName"
+            type="text"
+            name="userName"
+            value={userName}
+            onChange={(e) => handleValueChange(e, "userName")}
+          ></TextField>
+          <br />
+          <TextField
+            label="birthday"
+            type="text"
+            name="birthday"
+            value={birthday}
+            onChange={(e) => handleValueChange(e, "birthday")}
+          ></TextField>
+          <br />
+          <TextField
+            label="gender"
+            type="text"
+            name="gender"
+            value={gender}
+            onChange={(e) => handleValueChange(e, "gender")}
+          ></TextField>
+          <br />
+          <TextField
+            label="job"
+            type="text"
+            name="job"
+            value={job}
+            onChange={(e) => handleValueChange(e, "job")}
+          ></TextField>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleFormSubmit}
+          >
+            Add
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    // <form onSubmit={handleFormSubmit}>
+    //   <h1>Add Customer</h1>
+    //   Profile Image:{" "}
+    //   <input
+    //     type="file"
+    //     name="file"
+    //     file={file}
+    //     value={fileName}
+    //     onChange={handleFileChange}
+    //   ></input>
+    //   <br />
+    //   Name:{" "}
+    //   <input
+    //     type="text"
+    //     name="userName"
+    //     value={userName}
+    //     onChange={(e) => handleValueChange(e, "userName")}
+    //   ></input>
+    //   <br />
+    //   Birthday:{" "}
+    //   <input
+    //     type="text"
+    //     name="birthday"
+    //     value={birthday}
+    //     onChange={(e) => handleValueChange(e, "birthday")}
+    //   ></input>
+    //   <br />
+    //   Sex:{" "}
+    //   <input
+    //     type="text"
+    //     name="gender"
+    //     value={gender}
+    //     onChange={(e) => handleValueChange(e, "gender")}
+    //   ></input>
+    //   <br />
+    //   Job:{" "}
+    //   <input
+    //     type="text"
+    //     name="job"
+    //     value={job}
+    //     onChange={(e) => handleValueChange(e, "job")}
+    //   ></input>
+    //   <br />
+    //   <button type="submit">Add</button>
+    // </form>
   );
 };
 
--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const CustomerDelete = ({ id, stateRefresh }) => {
+  const [open, setOpen] = useState(false); //dialog 창이 열려있는지 체크
+
+  const initState = () => {
+    setOpen(false);
+  };
+
+  //고객 추가 모달 창이 열리면
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  //고객 추가 모달 창이 닫히면
+  const handleClose = () => {
+    initState();
+  };
+
   const deleteCustomer = (id) => {
     const url = "/api/customers/" + id;
     fetch(url, {
@@ -9,7 +32,31 @@ const CustomerDelete = ({ id, stateRefresh }) => {
     stateRefresh();
   };
 
-  return <button onClick={(e) => deleteCustomer(id)}>Delete</button>;
+  return (
+    <div>
+      <Button variant="contained" color="secondary" onClick={handleClickOpen}>
+        Delete
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Warning Delete!</DialogTitle>
+        <DialogContent>
+          <Typography gutterBottom>Delete Selected Customer</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={(e) => deleteCustomer(id)}
+          >
+            Delete
+          </Button>
+          <Button variant="outlined" color="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 };
 
 export default CustomerDelete;
